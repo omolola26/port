e7b2e67bbbe5224d7e4c2cdee45f0627efaa4887
unknown <omololaalimat22@gmail.com>
2022-04-26 23:49:41 +0100

    corrected

--- src/Component/PortfolioFive.js
+++ src/Component/PortfolioFive.js
@@ -18,7 +18,7 @@ const PortfolioFive = () => {
            <img className="port_img" src={Five} alt="five"/>
            </div>
            <div className="port_button">
-           <button type="submit"><a href="https://elated-sinoussi-b2073f.netlify.app/"  rel="noreferrer" target="_blank" className="imgIcon">LIFE</a></button>
+           <button type="submit"><a href="https://elated-sinoussi-b2073f.netlify.app/"  rel="noreferrer" target="_blank" className="imgIcon">LIVE</a></button>
            <button type="submit" className="life"><a href="https://github.com/omolola26/gmy"  rel="noreferrer" target="_blank"><Code/></a></button>
            </div>
           </div>
--- src/Component/PortfolioThree.js
+++ src/Component/PortfolioThree.js
@@ -16,7 +16,7 @@ function PortfolioThree(){
            <img className="port_img" src={Three} alt="three"/>
            </div> 
           <div className="port_button">
-           <button type="submit"><a href="https://suspicious-torvalds-50b5fd.netlify.app/" rel="noreferrer" target="_blank" className="imgIcon">LIFE</a></button>
+           <button type="submit"><a href="https://suspicious-torvalds-50b5fd.netlify.app/" rel="noreferrer" target="_blank" className="imgIcon">LIVE</a></button>
            <button type="submit" className="life"><a href="https://github.com/omolola26/spicy" rel="noreferrer" target="_blank"><Code/></a></button>
            </div>
           </div>
